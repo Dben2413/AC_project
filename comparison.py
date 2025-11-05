ad451d2e854d34d154d51447af92736179a79868
Dben2413 <fj22767@bristol.ac.uk>
2025-11-05 05:02:13 +0000

    1

---
+++ comparison.py
@@ -0,0 +1,24 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+
+
+
+run_time = [0.016996,0.047707,0.175356,0.374829,0.542549,0.703961]
+size = [20,40,80,120,140,160]
+fig,ax1 =plt.subplots()
+ax1.plot(size,run_time,marker = ".",label="runtime",color = "blue")
+ax1.set_ylabel("run time(s)")
+ax1.set_xlabel("size of grid")
+ax1.legend()
+plt.show()
+
+y = np.log(np.array(run_time))
+x = np.log(np.array(size)) 
+fig,ax1 =plt.subplots()
+ax1.plot(x,y,marker = ".",label="log runtime",color = "black")
+ax1.set_ylabel("log(run time(s))")
+ax1.set_xlabel("log(size of grid)")
+ax1.legend()
+plt.show()
