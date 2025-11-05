4e2a22f20f51b13f19a11f89984deba2c92e812c
Dben2413 <fj22767@bristol.ac.uk>
2025-11-05 06:12:09 +0000

    final update

--- comparison.py
+++ comparison.py
@@ -13,20 +13,20 @@ ax1.legend()
 plt.show()
 
 
-# run_time = [0.189916,0.593003,2.198197,5.016885,8.647348,19.044871,37.389928]
-# size = [20,40,80,120,160,240,320]
-# fig,ax1 =plt.subplots()
-# ax1.plot(size,run_time,marker = ".",label="runtime",color = "blue")
-# ax1.set_ylabel("run time(s)")
-# ax1.set_xlabel("size of grid")
-# ax1.legend()
-# plt.show()
+run_time = [0.189916,0.593003,2.198197,5.016885,6.680077,8.647348]
+size = [20,40,80,120,140,160]
+fig,ax1 =plt.subplots()
+ax1.plot(size,run_time,marker = ".",label="runtime",color = "blue")
+ax1.set_ylabel("run time(s)")
+ax1.set_xlabel("size of grid")
+ax1.legend()
+plt.show()
 
-# y = np.log(np.array(run_time))
-# x = np.log(np.array(size)) 
-# fig,ax1 =plt.subplots()
-# ax1.plot(x,y,marker = ".",label="log runtime",color = "black")
-# ax1.set_ylabel("log(run time(s))")
-# ax1.set_xlabel("log(size of grid)")
-# ax1.legend()
-# plt.show()
+y = np.log(np.array(run_time))
+x = np.log(np.array(size)) 
+fig,ax1 =plt.subplots()
+ax1.plot(x,y,marker = ".",label="log runtime",color = "black")
+ax1.set_ylabel("log(run time(s))")
+ax1.set_xlabel("log(size of grid)")
+ax1.legend()
+plt.show()
