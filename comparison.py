9754afb4955a5b5126935bdcde1db1ca851bae6f
Dben2413 <fj22767@bristol.ac.uk>
2025-11-05 04:26:25 +0000

    graph

--- comparison.py
+++ comparison.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import numpy as np
 
 
 
@@ -6,8 +7,26 @@ run_time = [7.70,2.95,1.93,1.52,2.14,2.029609,2.253543]
 cores = [2,4,6,8,10,12,14]
 fig,ax1 =plt.subplots()
 ax1.plot(cores,run_time,marker = ".",label="runtime",color = "red")
-ax1.set_ylabel("number of cores")
-ax1.set_xlabel("run time")
+ax1.set_xlabel("number of cores")
+ax1.set_ylabel("run time(s)")
 ax1.legend()
 plt.show()
-# plt.savefig("MPI_performance_comparison.pdf", format="pdf", bbox_inches="tight")
+
+
+# run_time = [0.189916,0.593003,2.198197,5.016885,8.647348,19.044871,37.389928]
+# size = [20,40,80,120,160,240,320]
+# fig,ax1 =plt.subplots()
+# ax1.plot(size,run_time,marker = ".",label="runtime",color = "blue")
+# ax1.set_ylabel("run time(s)")
+# ax1.set_xlabel("size of grid")
+# ax1.legend()
+# plt.show()
+
+# y = np.log(np.array(run_time))
+# x = np.log(np.array(size)) 
+# fig,ax1 =plt.subplots()
+# ax1.plot(x,y,marker = ".",label="log runtime",color = "black")
+# ax1.set_ylabel("log(run time(s))")
+# ax1.set_xlabel("log(size of grid)")
+# ax1.legend()
+# plt.show()
